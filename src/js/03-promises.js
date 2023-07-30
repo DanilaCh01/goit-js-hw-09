021c947edc9cbe59b5ac6cd69e6365ea8198e690
DanilaCh01 <117128628+DanilaCh01@users.noreply.github.com>
2023-07-30 20:22:04 +0300

    fixed work

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,39 +1,43 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const delay = document.querySelector('[name="delay"]');
-const step = document.querySelector('[name="step"]');
-const amount = document.querySelector('[name="amount"]');
-const form = document.querySelector('.form');
+const refs = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  form: document.querySelector('.form'),
+};
 
-form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('submit', onFormSbmt);
 
-function onFormSubmit(event) {
-  event.preventDefault();
+function onFormSbmt(evt) {
+  evt.preventDefault();
+
   const {
     elements: { delay, step, amount },
-  } = event.currentTarget;
+  } = evt.currentTarget;
+
   if (+delay.value < 0 || +step.value < 0 || +amount.value < 0) {
     Notify.failure('Please, enter only positive values')
     return
-  }
+  };
 
   for (let i = 0; i < +amount.value; i += 1) {
-    const sumDelay = +delay.value + +step.value * i;
-    createPromise(i, sumDelay)
+    const countedDelay = +delay.value + +step.value * i;
+
+    createPromise(i, countedDelay)
       .then(result => Notify.success(result))
       .catch(error => Notify.failure(error));
   };
 };
 
-
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
+  return new Promise((res, rej) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        res(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        rej(`❌ Rejected promise ${position} in ${delay}ms`);
       }
     }, delay);
   });
